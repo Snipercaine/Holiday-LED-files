03a33d0a1d540a1d6270842a520b325fa2556cc7
Mark Macherey <markmacherey@gmail.com>
2018-12-02 13:06:07 -0600

    Changed folder structure

---
+++ LED_ez_Pub/Definitions.h
@@ -0,0 +1,331 @@
+///////////////DrZzs Palettes for custom BPM effects//////////////////////////
+///////////////Add any custom palettes here//////////////////////////////////
+
+// Gradient palette "bhw2_thanks_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_thanks.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 36 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_thanks_gp ) {
+    0,   9,  5,  1,
+   48,  25,  9,  1,
+   76, 137, 27,  1,
+   96,  98, 42,  1,
+  124, 144, 79,  1,
+  153,  98, 42,  1,
+  178, 137, 27,  1,
+  211,  23,  9,  1,
+  255,   9,  5,  1};
+
+// Gradient palette "bhw2_redrosey_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_redrosey.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 32 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_redrosey_gp ) {
+    0, 103,  1, 10,
+   33, 109,  1, 12,
+   76, 159,  5, 48,
+  119, 175, 55,103,
+  127, 175, 55,103,
+  178, 159,  5, 48,
+  221, 109,  1, 12,
+  255, 103,  1, 10};
+
+// Gradient palette "bluered_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/h5/tn/bluered.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bluered_gp ) {
+    0,   0,  0,255,
+  127, 255,255,255,
+  255, 255,  0,  0};
+
+// Gradient palette "bhw2_xmas_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_xmas.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 48 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_xmas_gp ) {
+    0,   0, 12,  0,
+   40,   0, 55,  0,
+   66,   1,117,  2,
+   77,   1, 84,  1,
+   81,   0, 55,  0,
+  119,   0, 12,  0,
+  153,  42,  0,  0,
+  181, 121,  0,  0,
+  204, 255, 12,  8,
+  224, 121,  0,  0,
+  244,  42,  0,  0,
+  255,  42,  0,  0};
+
+// Gradient palette "bhw2_xc_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_xc.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_xc_gp ) {
+    0,   4,  2,  9,
+   58,  16,  0, 47,
+  122,  24,  0, 16,
+  158, 144,  9,  1,
+  183, 179, 45,  1,
+  219, 220,114,  2,
+  255, 234,237,  1};
+
+// Gradient palette "bhw1_04_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_04.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw1_04_gp ) {
+    0, 229,227,  1,
+   15, 227,101,  3,
+  142,  40,  1, 80,
+  198,  17,  1, 79,
+  255,   0,  0, 45};
+
+// Gradient palette "bhw4_051_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_051.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 36 bytes of program space.
+
+// Gradient palette "fs2006_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/cl/tn/fs2006.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 56 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( fs2006_gp ) {
+    0,   0, 49,  5,
+   34,   0, 49,  5,
+   34,  79,168, 66,
+   62,  79,168, 66,
+   62, 252,168, 92,
+  103, 252,168, 92,
+  103, 234, 81, 29,
+  143, 234, 81, 29,
+  143, 222, 30,  1,
+  184, 222, 30,  1,
+  184,  90, 13,  1,
+  238,  90, 13,  1,
+  238, 210,  1,  1,
+  255, 210,  1,  1};
+
+
+DEFINE_GRADIENT_PALETTE( bhw4_051_gp ) {
+    0,   1,  1,  4,
+   28,  16, 24, 77,
+   66,  35, 87,160,
+  101, 125,187,205,
+  127, 255,233, 13,
+  145, 125,187,205,
+  193,  28, 70,144,
+  224,  14, 19, 62,
+  255,   1,  1,  4};
+
+// Gradient palette "blue_g2_5_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/go2/webtwo/tn/blue-g2-5.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 16 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( blue_g2_5_gp ) {
+    0,   2,  6, 63,
+  127,   2,  9, 67,
+  255,   255, 255, 115,
+  255,   255, 255, 0};
+
+// Gradient palette "bhw3_41_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw3/tn/bhw3_41.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 36 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw3_41_gp ) {
+    0,   0,  0, 45,
+   71,   7, 12,255,
+   76,  75, 91,255,
+   76, 255,255,255,
+   81, 255,255,255,
+  178, 255,255,255,
+  179, 255, 55, 45,
+  196, 255,  0,  0,
+  255,  42,  0,  0};
+
+DEFINE_GRADIENT_PALETTE( test_gp ) {
+    0,  255,  0,  0, // Red
+// 32,  171, 85,  0, // Orange
+// 64,  171,171,  0, // Yellow
+// 96,    0,255,  0, // Green
+//128,    0,171, 85, // Aqua
+  160,    0,  0,255, // Blue
+//192,   85,  0,171, // Purple
+//224,  171,  0, 85, // Pink
+//255,  255,  0,  0};// and back to Red
+};  
+
+// Gradient palette "bhw2_greenman_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_greenman.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_greenman_gp ) {
+    0,   1, 22,  1,
+  130,   1,168,  2,
+  255,   1, 22,  1};
+
+// Gradient palette "Pills_3_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/ds/icons/tn/Pills-3.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Pills_3_gp ) {
+    0,   4, 12,122,
+  127,  55, 58, 50,
+  255, 192,147, 11};
+
+// Gradient palette "Orange_to_Purple_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/ds/icons/tn/Orange-to-Purple.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Orange_to_Purple_gp ) {
+    0, 208, 50,  1,
+  127, 146, 27, 45,
+  255,  97, 12,178};
+
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+String setColor ="0,0,150";
+String setPower;
+String setEffect = "Solid";
+String setBrightness = "150";
+int brightness = 150;
+String setAnimationSpeed;
+int animationspeed = 240;
+String setColorTemp;
+int Rcolor = 0;
+int Gcolor = 0;
+int Bcolor = 0;
+CRGB leds[NUM_LEDS8];
+char mcuHostName[64]; 
+char lwtTopic[96];  
+char colorstatusPubTopic[96];    
+char setcolorSubTopic[96];  
+char setpowerSubTopic[96];   
+char seteffectSubTopic[96];   
+char setbrightnessTopic[96];   
+char setcolorPubTopic[96];   
+char setpowerPubTopic[96];   
+char seteffectPubTopic[96];   
+char setbrightnessPubTopic[96];   
+char setanimationspeedTopic[96];   
+String mqttClientId; 
+bool Mqttconnected = 0 ;
+char NumberLEDUser[6]= "2";
+long numberLEDs = 0;
+char LED_TYPEUSER[10] ="WS2811";
+
+/****************FOR CANDY CANE-like desings***************/
+CRGBPalette16 currentPalettestriped; //for Candy Cane
+CRGBPalette16 hailPalettestriped; //for Hail
+CRGBPalette16 ThxPalettestriped; //for Thanksgiving
+CRGBPalette16 HalloweenPalettestriped; //for Halloween
+CRGBPalette16 HJPalettestriped; //for Holly Jolly
+CRGBPalette16 IndPalettestriped; //for Independence
+CRGBPalette16 gPal; //for fire
+
+/****************FOR NOISE - I'm using this one for Easter***************/
+static uint16_t dist;         // A random number for our noise generator.
+uint16_t scale = 30;          // Wouldn't recommend changing this on the fly, or the animation will be really blocky.
+uint8_t maxChanges = 48;      // Value for blending between palettes.
+CRGBPalette16 targetPalette(OceanColors_p);
+CRGBPalette16 currentPalette(CRGB::Black);
+
+/*****************For TWINKLE********/
+#define DENSITY     80
+int twinklecounter = 0;
+
+/*********FOR RIPPLE***********/
+uint8_t colour;                                               // Ripple colour is randomized.
+int center = 0;                                               // Center of the current ripple.
+int step = -1;                                                // -1 is the initializing step.
+uint8_t myfade = 255;                                         // Starting brightness.
+#define maxsteps 16                                           // Case statement wouldn't allow a variable.
+uint8_t bgcol = 0;                                            // Background colour rotates.
+int thisdelay = 20;                                           // Standard delay value.
+
+/**************FOR RAINBOW***********/
+uint8_t thishue = 0;                                          // Starting hue value.
+uint8_t deltahue = 10;
+
+/**************FOR DOTS**************/
+uint8_t   count =   0;                                        // Count up to 255 and then reverts to 0
+uint8_t fadeval = 224;                                        // Trail behind the LED's. Lower => faster fade.
+uint8_t bpm = 30;
+
+/**************FOR LIGHTNING**************/
+uint8_t frequency = 50;                                       // controls the interval between strikes
+uint8_t flashes = 8;                                          //the upper limit of flashes per strike
+unsigned int dimmer = 1;
+uint8_t ledstart;                                             // Starting location of a flash
+uint8_t ledlen;
+int lightningcounter = 0;
+
+/********FOR FUNKBOX EFFECTS**********/
+int idex = 0;                //-LED INDEX (0 to NumberLEDUser-1
+int TOP_INDEX = int(String(NumberLEDUser).toInt() / 2);
+int thissat = 255;           //-FX LOOPS DELAY VAR
+
+//////////////////add thishue__ for Police All custom effects here/////////////////////////////////////////////////////////
+/////////////////use hsv Hue number for one color, for second color change "thishue__ + __" in the setEffect section//////
+
+uint8_t thishuepolice = 0;
+uint8_t thishuehail = 64;
+uint8_t thishueLovey = 0;     
+int antipodal_index(int i) {
+  int iN = i + TOP_INDEX;
+  if (i >= TOP_INDEX) {
+    iN = ( i + TOP_INDEX ) % String(NumberLEDUser).toInt();
+  }
+  return iN;
+}
+
+/********FIRE**********/
+#define COOLING  55
+#define SPARKING 120
+bool gReverseDirection = false;
+
+/********BPM**********/
+uint8_t gHue = 0;
+char message_buff[100];
+//#define FASTLED_INTERRUPT_RETRY_COUNT 0
+
+WiFiServer telnetServer(23);
+WiFiClient telnetClient;
+ESP8266HTTPUpdateServer httpUpdater;
+
+
+char wifiConfigAP[19]; 
+char wifiConfigPass[9];                          // AP config password, always 8 chars + NUL
+bool shouldSaveConfig = false;                   // Flag to save json config to SPIFFS
+const char LED_STYLE[] = "<style>button{background-color:#03A9F4;}body{width:60%;margin:auto;}</style>";
+
+WiFiClient wifiClient;
+ESP8266WebServer webServer(80);
+
+char wifiSSID[32] = ""; // Leave unset for wireless autoconfig.
+char wifiPass[64] = ""; // Note that these values will be lost if auto-update is used,
+
+const char *getDeviceID() {
+  char *identifier = new char[30];
+  os_strcpy(identifier, espName);
+  strcat_P(identifier, PSTR("-"));
+
+  char cidBuf[7];
+  sprintf(cidBuf, "%06X", ESP.getChipId());
+  os_strcat(identifier, cidBuf);
+
+  return identifier;
+}
